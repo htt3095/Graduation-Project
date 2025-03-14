8ab8caed020a8a6e3e36d81fcb5ef495f4a20f6a
htt3095 <hhtt3095@gmail.com>
2025-03-14 10:47:07 +0700

    Added sidebar for settings

---
+++ code.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
