7f74145b5b6c4fb80b8535b31038616c989d0af6
htt3095 <hhtt3095@gmail.com>
2025-03-14 11:02:09 +0700

    Added Categories sidebar

--- code.js
+++ code.js
@@ -7,3 +7,13 @@ function closeNav() {
     document.getElementById("sidebar").style.width = "0";
     document.body.style.backgroundColor = "white";
 }
+
+function openNav2() {
+    document.getElementById("sidebar2").style.width = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav2() {
+    document.getElementById("sidebar2").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
